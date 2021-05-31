69418350cf361036e571d6704acae8d0282b216d
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-05-31 15:01:46 -0400

    don

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideHeight){
   // Write your code here!
-}
+  if(rideHeight <= 400) {
+    return 'This one is on me!'
+  } else if (rideHeight > 2000 && rideHeight <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (rideHeight > 2500) {
+    return 'No can do.'
+  };
+};
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
-}
+  return (someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.')
+};
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipDescrip){
   // Write your code here!
-}
+  switch (tipDescrip) {
+  case 'generous':
+    return 'Thank you so much.'
+  case 'not as generous':
+    return 'Thank you.'
+  default:
+    return 'Bye.'
+  };
+};
